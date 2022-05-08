1bb4799247ebc6d482f600aa93dcc5d7fa0d0f2d
Tien Tran <tientran2290@gmail.com>
2022-05-08 13:34:08 -0700

    Added logout

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -25,6 +25,12 @@ export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
 
 })
 
+// Logout user
+export const logout = createAsyncThunk('auth/logout', async () => { 
+  await authService.logout()
+  
+})
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -46,14 +52,15 @@ export const authSlice = createSlice({
         state.isSuccess = true
         state.user = action.payload
       })
-     
       .addCase(register.rejected, (state, action) => { 
         state.isLoading = false
         state.isError = true
         state.message = action.payload
         state.user = null
       })
-     
+      .addCase(logout.fulfilled, (state) => { 
+        state.user = null
+      })
   }
 })
 
