e5ba82c4420e6ae5b446fd0161a07723de6aa501
Tien Tran <tientran2290@gmail.com>
2022-05-10 15:39:30 -0700

    Added notes backend

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel.js')
-const Ticket = require('../models/ticketModal.js')
+const Ticket = require('../models/ticketModel.js')
 
 // @desc Get user tickets
 // @route GET /api/tickets
